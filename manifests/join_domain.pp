90a646d1d795a5bfb1c8add76b07cb0d41d45cf1
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 17:52:22 +0200

    Update join_domain.pp

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,5 +1,15 @@
 class devhops::join_domain {
   $joinpassword = lookup('devhops::windows_domain::join_password')
+  $query        = "inventory[facts] { facts.hostname ~ 'windchops' }"
+  $response     = puppetdb_query($query)
+  $dcip         = $response[0]['facts']['ipaddress']
+
+  dsc_xdnsserveraddress { 'dnsserveraddress':
+    dsc_address        => $dcip,
+    dsc_interfacealias => 'ethernet',
+    dsc_addressfamily  => 'ipv4',
+    before             => Dsc_xComputer['JoinDomain']
+  }
 
   dsc_xComputer { 'JoinDomain':
     dsc_name       => $trusted['certname'],
