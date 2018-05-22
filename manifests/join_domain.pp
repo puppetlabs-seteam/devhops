b9a3fb03ac010edd83fac6f6110c051e97257142
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 17:40:35 +0200

    Update join_domain.pp

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,10 +1,12 @@
 class devhops::join_domain {
+  $joinpassword = lookup('devhops::windows_domain::join_password')
+
   dsc_xComputer { 'JoinDomain':
     dsc_name       => $trusted['certname'],
-    dsc_domainname => $domainname,
+    dsc_domainname => lookup('devhops::windows_domain::name'),
     dsc_credential => {
-      'user'     => 'Administrator',
-      'password' => Sensitive($localadminpw)
+      'user'     => lookup('devhops::windows_domain::join_user'),
+      'password' => Sensitive($joinpassword)
     }
   }
 }
