54105be282e753852ec2c4b3ebab923505901a55
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 17:33:35 +0200

    changed to xComputer resource

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,5 +1,6 @@
 class devhops::join_domain {
-  dsc_xDSCDomainjoin { 'JoinDomain':
+  dsc_xComputer { 'JoinDomain':
+    dsc_name       => $trusted['certname']
     dsc_domain     => $domainname,
     dsc_credential => {
       'user'     => 'Administrator',
