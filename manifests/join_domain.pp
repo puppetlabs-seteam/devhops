3751508176f53c2d153b3c16cc4a125e4d14cc79
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 17:35:35 +0200

    Update join_domain.pp

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,7 +1,7 @@
 class devhops::join_domain {
   dsc_xComputer { 'JoinDomain':
     dsc_name       => $trusted['certname'],
-    dsc_domain     => $domainname,
+    dsc_domainname => $domainname,
     dsc_credential => {
       'user'     => 'Administrator',
       'password' => Sensitive($localadminpw)
