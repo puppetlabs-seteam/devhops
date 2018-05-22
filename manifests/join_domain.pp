304b278067c8cefa9b561505f75b4e508c116289
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-22 16:51:43 +0200

    Update join_domain.pp

--- manifests/join_domain.pp
+++ manifests/join_domain.pp
@@ -1,11 +1,9 @@
 class devhops::join_domain {
-  # this requires the following module to be set in the Puppetfile:
-  # mod 'trlinkin/domain_membership', '1.1.2'
-  # 
-  class { 'domain_membership':
-    domain       => lookup('devhops::windows_domain::name'),
-    username     => lookup('devhops::windows_domain::join_user'),
-    password     => lookup('devhops::windows_domain::join_password'),
-    join_options => '3',
+  dsc_xDSCDomainjoin { 'JoinDomain':
+    dsc_domain     => $domainname,
+    dsc_credential => {
+      'user'     => 'Administrator',
+      'password' => Sensitive($localadminpw)
+    }
   }
 }
